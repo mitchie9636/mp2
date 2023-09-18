d6121afec543f1f6e349bea75262f5b32c48347b
mitchie9636 <mitch.arroyo0222@gmail.com>
2023-09-18 23:29:51 +0800

    update 9-18-23

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const express = require('express');
-require('dotenv').config();
-const ejs = require('ejs');
-const path = require('path');
+const express = require('express');  //---------------------------
+require('dotenv').config();          //---------------------
+const ejs = require('ejs');          //-------------------
+const path = require('path');         //-------------------
 const router = require('./router');
-// const bodyParser = require('body-parser');
-const app = express();
+const bodyParser = require('body-parser');  //------------------
+const app = express();                            //--------------------------------------
 app.use(express.static('public'));
 
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
+                                        //---------directories-------referring-public & views folder------------------
 app.set('view engine', 'ejs');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 
+//--------end-directories--------------------------
+
+// const jobs = [];
 
 
-const jobs = [];
+//   app.post('/', (req, res) => {
+//     const firstName = req.body.firstName;
+//     const lastName = req.body.lastName;
+//     const email = req.body.email;
+//     const password = req.body.password;
 
-
-  app.post('/', (req, res) => {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    res.redirect('/thank-you');
-  });
-
+//     res.redirect('/thank-you');
+//   });
+                                      //--------------------------------
 app.get('/', (req, res)=>{
     res.render('main');
 });
 
-app.get('/', (req, res)=>{
-  res.render('findjobs');
-});
 
 
-//set the routes    
+//set the routes -----------------------------------   
 app.use('/', router)
-
+                                                                           //-----------------------------------------
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at http://${process.env.HOSTNAME}:${process.env.PORT}.`);
 });
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------
--- router.js
+++ router.js
@@ -6,87 +6,74 @@ const {check, validationResult} = require('express-validator');
 const router = express.Router();
 
 
-// //route to the login page
-router.get('/main', (req, res)=>{
-    res.render('main',{title:'main Page'});
-});
 
-router.get('/findjobs', (req, res)=>{
-    res.render('findjobs',{title:'findjobs Page'});
-});
-
-
+                  //---login routes------------
 router.get('/login', (req, res)=>{
     res.render('login',{title:'Login Page'});
 });
 
-
-const credential = {
+const credential = {                                        
     email: 'TOS@test.com',
-    password: 'tRio1234'
+    password: 'Trio1234'
 };
 
-router.get('/signup', (req, res)=>{
-    res.render('signup',{title:'Signup Page'});
-});
+//route to authenticate a user
+router.post('/login', [
+    check('email')
+    .notEmpty()
+    .withMessage('email is required.'),
 
-// // //route to authenticate a user
-// // router.post('/login', [
-// //     check('email')
-// //     .notEmpty()
-// //     .withMessage('email is required.'),
+    check('password')
+    .notEmpty()
+    .withMessage('password is required.')
+    .isLength({min:8})
+    .withMessage('password must be at least 8 characters.')
+],(req, res)=>{
 
-// //     check('password')
-// //     .notEmpty()
-// //     .withMessage('password is required.')
-// //     .isLength({min:8})
-// //     .withMessage('password must be at least 8 characters.')
-// // ],(req, res)=>{
+    const errors = validationResult(req);
 
-// //     const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        console.log(errors);
+        // res.send({ errors: errors.array() });
+        res.render('login',{title:'Login Page', errors: 'Please check your input' });
+    }else{
+        if(req.body.email == credential.email && req.body.password == credential.password){
+            //create a session
+            req.session.user = req.body.email;
+            res.redirect('/dashboard');
+        }else{
+            res.render('login',{title:'Login Page', isInvalid: 'Your credential does not exist' });
+        }
+    }
 
-// //     if(!errors.isEmpty()){
-// //         console.log(errors);
-// //         // res.send({ errors: errors.array() });
-// //         res.render('login',{title:'Login Page', errors: 'Please check your input' });
-// //     }else{
-// //         if(req.body.email == credential.email && req.body.password == credential.password){
-// //             //create a session
-// //             req.session.user = req.body.email;
-// //             res.redirect('/dashboard');
-// //         }else{
-// //             res.render('login',{title:'Login Page', isInvalid: 'Your credential does not exist' });
-// //         }
-// //     }
-
-// //     // if(!req.body.email || !req.body.password){
-// //     //     res.send('email or password is required');
-// //     // }else{
+    if(!req.body.email || !req.body.password){
+        res.send('email or password is required');
+    }else{
        
       
-// //     // }
-// // });
+    }
+});
 
-// // //route to the dashboard
-// // router.get('/dashboard',(req, res)=>{
-// //     if(req.session.user){
-// //         res.render('dashboard',{title:'Dashboard', user: req.session.user });
-// //     }else{
-// //         res.send(403);
-// //     }
-// // });
+//route to the dashboard
+router.get('/dashboard',(req, res)=>{
+    if(req.session.user){
+        res.render('dashboard',{title:'Dashboard', user: req.session.user });
+    }else{
+        res.send(403);
+    }
+});
 
-// // //route to destroy the session
-// // router.get('/logout', (req, res)=>{
-// //     req.session.destroy(function(err){
-// //         if(err){
-// //             console.log(err);
-// //             res.send(err);
-// //         }else{
-// //             res.render('login', {title:'Login Page', logout:'Logout successfully!'});
-// //         }
-// //     })
-// // });
+//route to destroy the session
+router.get('/logout', (req, res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }else{
+            res.render('login', {title:'Login Page', logout:'Logout successfully!'});
+        }
+    })
+});
 
 
 module.exports = router;
